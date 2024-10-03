99934aa97110e4fdf41890561ba2ae088c4be2f6
JGCConway <justinconway022@gmail.com>
2024-10-03 14:31:19 -0500

    Stop All Audio Feature Release

---
+++ newComponent.js
@@ -0,0 +1,18 @@
+function stopAllAudio() {
+    wavesurfer.stop();
+    wavesurfer2.stop();
+    wavesurfer3.stop();
+    wavesurfer4.stop();
+    wavesurfer5.stop();
+    wavesurfer6.stop();
+    wavesurfer7.stop();
+    playBtn1.src = "media/play.png";
+    playBtn2.src = "media/play.png";
+    playBtn3.src = "media/play.png";
+    playBtn4.src = "media/play.png";
+    playBtn5.src = "media/play.png";
+    playBtn6.src = "media/play.png";
+    playBtn7.src = "media/play.png";
+}
+
+document.getElementById('stopAllAudio').addEventListener('click', stopAllAudio);
